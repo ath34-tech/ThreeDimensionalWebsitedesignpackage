e51cd2ef610b977731d518cdd54ba6bcea7c3c44
bowser <82615765+uwu404@users.noreply.github.com>
2021-08-13 14:15:34 +0900

    added navbar

---
+++ navbar/navbar.jsx
@@ -0,0 +1,31 @@
+import '../CSS/animation.css'
+import '../CSS/navbar.css'
+import React from 'react'
+import { useState } from 'react'
+
+
+const Navbar = ({ style, showInput, color }) => {
+  const [showNavBar, setShowNavBar] = useState(false)
+  const [indexOfClickedButton, setIndexOfClickedButton] = useState(0)
+  const isClicked = (num) => indexOfClickedButton === num && "clicked"
+
+  const click = (index) => () => setIndexOfClickedButton(index)
+  const showNav = () => setShowNavBar(prev => !prev)
+
+  return (
+    <nav style={style} className={`navbar ${showNavBar && "clicked"}`}>
+      <div onClick={showNav}  className="white-line"></div>
+      <div className="logo">LOGO</div>
+      <input placeholder="search" className="search" />
+      <div style={{ color }} className="buttons">
+        <a href="#"><div onClick={click(0)} className={"home " + isClicked(0)}>Home</div></a>
+        <a href="#"><div onClick={click(1)} className={"About " + isClicked(1)}>About</div></a>
+        <a href="#"><div onClick={click(2)} className={"contact " + isClicked(2)}>Contact</div></a>
+        <a href="#"><div onClick={click(3)} className={"work " + isClicked(3)}>Work</div></a>
+        <a href="#"><div onClick={click(4)} className={"portfolio " + isClicked(4)}>Portfolio</div></a>
+      </div>
+    </nav>
+  )
+}
+
+export default Navbar
